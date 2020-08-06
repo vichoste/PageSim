2d628696bc768f2ebff84fa0103167cd3e7ea0a9
Vicente <thunderwearsvicho37@protonmail.com>
2020-08-06 19:56:48 -0400

    Params

--- PageSim/Program.cs
+++ PageSim/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommandLine;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,18 @@ using System.Threading.Tasks;
 
 namespace PageSim {
 	class Program {
+		public class Options {
+			[Option('f', Required = true, HelpText = "Archivo que contiene la secuencia de páginas")]
+			public string PageSequence { get; set; }
+			[Value(0, Required = true, MetaName = "Cantidad de páginas", HelpText = "Establece la cantidad de páginas que contendrá la memoria virtual")]
+			public byte PageCount { get; set; } 
+			[Value(0, Required = true, MetaName = "Espacio de memoria virtual disponible [kb]", HelpText = "Establece la cantidad de espacio de memoria virtual en kilobytes")]
+			public int VirtualMemoryAmount { get; set; }
+		}
+		/// <summary>
+		/// Main function.
+		/// </summary>
+		/// <param name="args">Input arguments</param>
 		static void Main(string[] args) {
 		}
 	}
