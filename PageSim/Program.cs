b1e111a66aedbff332c5a9aa5b43b73c0380c5f8
Vicente <thunderwearsvicho37@protonmail.com>
2020-08-06 20:06:48 -0400

    Params 2

--- PageSim/Program.cs
+++ PageSim/Program.cs
@@ -7,19 +7,25 @@ using System.Threading.Tasks;
 
 namespace PageSim {
 	class Program {
-		public class Options {
+		class Options {
 			[Option('f', Required = true, HelpText = "Archivo que contiene la secuencia de páginas")]
 			public string PageSequence { get; set; }
-			[Value(0, Required = true, MetaName = "Cantidad de páginas", HelpText = "Establece la cantidad de páginas que contendrá la memoria virtual")]
+			[Value(1, Required = true, MetaName = "Cantidad de páginas", HelpText = "Establece la cantidad de páginas que contendrá la memoria virtual")]
 			public byte PageCount { get; set; } 
-			[Value(0, Required = true, MetaName = "Espacio de memoria virtual disponible [kb]", HelpText = "Establece la cantidad de espacio de memoria virtual en kilobytes")]
+			[Value(2, Required = true, MetaName = "Espacio de memoria virtual disponible [kb]", HelpText = "Establece la cantidad de espacio de memoria virtual en kilobytes")]
 			public int VirtualMemoryAmount { get; set; }
+		}
+		/// <summary>
+		/// Starts the program.
+		/// </summary>
+		/// <param name="options">Input parameters</param>
+		static void RunOptions(Options options) {
+
 		}
 		/// <summary>
 		/// Main function.
 		/// </summary>
 		/// <param name="args">Input arguments</param>
-		static void Main(string[] args) {
-		}
+		static void Main(string[] args) => _ = Parser.Default.ParseArguments<Options>(args).WithParsed(RunOptions);
 	}
 }
