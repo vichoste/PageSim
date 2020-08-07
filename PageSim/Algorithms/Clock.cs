3b2fe964f40fec36f5fc8cd6ae62c0f3eea3e429
Vicente <thunderwearsvicho37@protonmail.com>
2020-08-07 15:22:40 -0400

    Interface rework

--- PageSim/Algorithms/Clock.cs
+++ PageSim/Algorithms/Clock.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PageSim.Structures;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,6 @@ using System.Threading.Tasks;
 
 namespace PageSim.Algorithms {
 	public class Clock : IAlgorithmStrategy {
-		public void Execute() => throw new NotImplementedException();
+		public void Execute(VirtualMemory virtualMemory, string[] pageSequence) => throw new NotImplementedException();
 	}
 }
--- PageSim/Algorithms/IAlgorithmStrategy.cs
+++ PageSim/Algorithms/IAlgorithmStrategy.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PageSim.Structures;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,6 @@ using System.Threading.Tasks;
 
 namespace PageSim.Algorithms {
 	public interface IAlgorithmStrategy {
-		void Execute();
+		void Execute(VirtualMemory virtualMemory, string[] pageSequence);
 	}
 }
