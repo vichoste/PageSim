8f03688eefc8194e299bfb897f1cc65331c1b092
Vicente <thunderwearsvicho37@protonmail.com>
2020-08-07 17:45:17 -0400

    New LRU

--- PageSim/Algorithms/LeastRecentlyUsed.cs
+++ PageSim/Algorithms/LeastRecentlyUsed.cs
@@ -1,5 +1,6 @@
 ﻿using PageSim.Structures;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,21 @@ namespace PageSim.Algorithms {
 		/// <param name="virtualMemory">Virtual memory to use</param>
 		/// <param name="pageSequence">Page sequence to use</param>
 		/// <returns>Amount of misses</returns>
-		public int Execute(VirtualMemory virtualMemory, string[] pageSequence) => throw new NotImplementedException();
+		public int Execute(VirtualMemory virtualMemory, string[] pageSequence) {
+			var hashtable = new Hashtable();
+			var missCount = 0;
+			var i = 0;
+			foreach (var page in pageSequence) {
+				if (!hashtable.ContainsKey(page)) {
+					hashtable.Add(page, 1);
+				}
+				// While the queue is not full
+				if (i < virtualMemory.PageCount) {
+					virtualMemory[i++] = page;
+					continue;
+				}
+			}
+			return missCount;
+		}
 	}
 }
