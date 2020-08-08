88456b0d1349a88bcc6ad082145a5a3d183f66c6
Vicente <thunderwearsvicho37@protonmail.com>
2020-08-07 23:10:09 -0400

    ClockList

---
+++ PageSim/Structures/ClockList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PageSim.Structures {
+	/// <summary>
+	/// Defines the structure for the clock
+	/// </summary>
+	public class ClockList {
+		private class Node {
+			private string _Page;
+			private bool _R;
+			public Node(string page) {
+				this._Page = page;
+				this._R = true;
+			}
+		}
+		public ClockList(int pageCount) {
+
+		}
+	}
+}
